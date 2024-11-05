3515f430706d2059dbe40ffc4692b5901526b53e
Cuong Nguyen <ngoccuong11789@gmail.com>
2024-11-05 08:37:06 -0600

    first commit

---
+++ SwiftUI-List-Starter/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Cuong Nguyen Ngoc on 04/11/2024.
+//
+
+import Foundation
---
+++ SwiftUI-List-Starter/VideoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoDetailView.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Cuong Nguyen Ngoc on 05/11/2024.
+//
+
+import SwiftUI
+
+struct VideoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoDetailView()
+}

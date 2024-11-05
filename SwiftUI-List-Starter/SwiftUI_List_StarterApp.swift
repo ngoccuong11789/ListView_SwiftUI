4745ee483e4ac8f33e5b17062690cda914ba6324
Cuong Nguyen <ngoccuong11789@gmail.com>
2024-11-04 20:29:16 -0600

    Initial Commit

---
+++ SwiftUI-List-Starter/SwiftUI_List_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_StarterApp.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Cuong Nguyen Ngoc on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
